74f2afe5038917643867f01c14db358b5fadb60e
abi <abitriesthings@gmail.com>
2024-12-15 18:09:05 -0800

    switch back to SF

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const weatherApiUrl = 'https://api.weather.gov/gridpoints/SEW/125,68/forecast';
+const weatherApiUrl = 'https://api.weather.gov/gridpoints/PQR/116,107/forecast';
 /* 
 Florida: https://api.weather.gov/gridpoints/SJU/270,64/forecast
 Fairfax: https://api.weather.gov/gridpoints/LWX/82,71/forecast
 Portland: https://api.weather.gov/gridpoints/PQR/113,104/forecast
-Seattle: https://api.weather.gov/gridpoints/SEW/125,68/forecast Showera
+Seattle: https://api.weather.gov/gridpoints/SEW/125,68/forecast Showers
 Boise: https://api.weather.gov/gridpoints/BOI/133,86/forecast
 Denver: https://api.weather.gov/gridpoints/BOU/71,60/forecast Cloudy
 Lincoln: https://api.weather.gov/gridpoints/OAX/56,40/forecast
